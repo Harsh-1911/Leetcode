3d73ef748e98fcac64e76f9aa0c1fad38bab88f3
Harsh-1911 <hpratap437@gmail.com>
2025-08-02 15:39:45 +0530

    Time: 0 ms (100%), Space: 44.5 MB (56.79%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,33 +13,27 @@
  *     }
  * }
  */
-class Solution {
+ class Solution {
     public boolean isBalanced(TreeNode root) {
-        return dfsHeight(root)!=-1;
+        if (root == null){
+            return true;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        if(Math.abs(leftHeight - rightHeight)>1){
+            return false;
+        }
+        return isBalanced(root.left)&& isBalanced(root.right);
     }
 
-    public  int dfsHeight(TreeNode root){
-            if(root==null){
-                return 0;
-            }
-
-                int leftHeight =dfsHeight(root.left);
-                if(leftHeight== -1){
-                    return -1;
-                }
-                    int rightHeight =dfsHeight(root.right);
-                    if(rightHeight==-1){
-                        return -1;
-                    }
-                    if(Math.abs(leftHeight-rightHeight)>1){
-                        return -1;
-
-                    }
-              return Math.max(leftHeight,rightHeight)+1;
-                    
-            
-
+        public static  int height(TreeNode node) {
+        if (node == null){
+            return 0;
         }
-        
-    
-}
+        int left = height(node.left);
+        int right = height(node.right);
+        return Math.max(left, right) + 
+        1;
+        }
+ }
