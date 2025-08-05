6603a0d86522df8dd2534c166e24860dd7c01572
Harsh-1911 <hpratap437@gmail.com>
2025-08-05 11:42:27 +0530

    Time: 0 ms (100%), Space: 41.9 MB (66.2%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        int count=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)
+            {
+                count++;
+            }
+            else if(nums[i]==1){
+                count1++;
+            }
+            else {
+                count2++;
+            }
+        }
+            int j=0;
+            for(int i =0;i<count;i++){
+                nums[j]=0;
+                j++;
+            }
+            for(int i =0;i<count1;i++){
+            nums[j]=1;
+            j++;
+        }
+          for(int i =0;i<count2;i++){
+            nums[j]=2;
+            j++;
+        }
+        
+    }
+}
