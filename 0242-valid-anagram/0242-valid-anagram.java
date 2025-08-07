1e878fca6a0415dfcc1ac5e13567cce1a05ec48d
Harsh-1911 <hpratap437@gmail.com>
2025-08-07 14:19:10 +0530

    Time: 6 ms (42.53%), Space: 45 MB (10.91%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static String sort(String a){
+        char c[]=a.toCharArray();
+        Arrays.sort(c);
+        return new String(c);
+    }
+
+
+    public   boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+
+        }
+        s=sort(s);
+        t=sort(t);
+        for (int i=0;i<s.length();i++){
+            if(s.charAt(i)!=t.charAt(i)){
+                return false;
+            }
+
+        }
+        return true;
+        
+    }
+}
