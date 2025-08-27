60be36e8b6fa6442b8349507c9d4526f1ac09c9d
Harsh-1911 <hpratap437@gmail.com>
2025-08-27 22:27:23 +0530

    Time: 10 ms (13.84%), Space: 78.6 MB (31.21%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,36 +1,33 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n = nums.length;
-        ArrayList<Integer> pos= new ArrayList<>();
-         ArrayList<Integer> neg= new ArrayList<>();
-         int j =0;
-         while (j<n)
-         {
-            if(nums[j]>0){
-                pos.add(nums[j]);
-                j++;
-            }
-            else {
-                neg.add(nums[j]);
-                j++;
-            }
-         }
-         int p=0;
-         int q=0;
-         for(int k=0;k<n;k++){
-            if(k%2==0){
-                nums[k]=pos.get(p);
-                p++;
-            }
-            else {
-                nums[k]=neg.get(q);
-                q++;
-            }
-         }
-         return nums ;
+        int n=  nums.length;
+        ArrayList<Integer>pos= new ArrayList<>();
+                ArrayList<Integer>neg= new ArrayList<>();
+                int j =0;
+                while(j<n){
+                    if(nums[j]>0){
+                        pos.add(nums[j]);
+                        j++;
+                    }
+                    else {
+                        neg.add(nums[j]);
+                        j++;
+                    }
+                }
+                int p=0;
+                int q=0;
+                for(int i =0;i<n;i++){
+                    if(i%2==0){
+                        nums[i]=pos.get(p);
+                        p++;
 
-
-         
+                    }
+                    else {
+                        nums[i]= neg.get(q);
+                        q++;
+                    }
+                }
+                return nums;
 
         
     }
