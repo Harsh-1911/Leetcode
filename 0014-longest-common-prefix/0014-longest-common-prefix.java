bd7e3299e7802152de38fea8d7e85512f7e6d674
Harsh-1911 <hpratap437@gmail.com>
2025-08-30 16:50:18 +0530

    Time: 1 ms (61.7%), Space: 42.1 MB (18.25%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,20 +1,20 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length==0){
+        if(strs== null && strs.length==0){
             return "";
+
         }
         Arrays.sort(strs);
         String strs1=strs[0];
-        String strs2=strs[strs.length-1];
-        int index =0;
+       String  strs2=strs[strs.length-1];
+       int index =0;
+       int  minlength=Math.min(strs1.length(),strs2.length());
         
-        int  minLength=Math.min(strs1.length(),strs2.length());
-        
-        while (index<minLength && strs1.charAt(index)==strs2.charAt(index)){
+        while (index<minlength && strs1.charAt(index)==strs2.charAt(index)){
             index++;
-
         }
         return strs1.substring(0,index);
+
         
     }
 }
