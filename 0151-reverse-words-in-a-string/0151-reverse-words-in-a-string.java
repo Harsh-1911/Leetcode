1d01b0cf9b9500fbca637698f5d99142e08f15df
Harsh-1911 <hpratap437@gmail.com>
2025-08-30 14:45:48 +0530

    Time: 17 ms (6.75%), Space: 45.4 MB (13.33%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,53 +1,54 @@
 class Solution {
-    public static void reverse(String[]words,int size){
-        int  i=0;
+    public static void reverse(String word[],int size){
+        int i =0; 
         int j =size-1;
         while(i<j){
-            String temp =words[i];
-            words[i]=words[j];
-            words[j]=temp;
+            String temp = word[i];
+            word[i]=word[j];
+            word[j]= temp ;
             i++;
             j--;
-                    }
+
+        }
+
 
     }
     public String reverseWords(String s) {
-        int n =s.length();
-        String words[]= new String[n];
-        int index=0;
-        String currentword="";
-        for(int i=0;i<n;i++){
-            char ch = s.charAt(i);
-            if(ch!=' '){
-                currentword=currentword + ch;
-
-            }
-            else {
-                 if (!currentword.equals("")) {
-            words[index] = currentword;
-                currentword="";
-                index ++;
-                 }
-
-            }
-        }
-         if (!currentword.equals("")) {
-            words[index] = currentword;
-            index++;
-         }
-          reverse(words, index);
-
-        String ans = "";
-        for (int i = 0; i < index; i++) {
-            ans += words[i];
-            if (i != index - 1) {
-                ans += " ";
-            }
-        }
-        return ans ;
-
-
-
+        int n = s.length();
+        String currentword ="";
         
+       
+        String[] word = new String[n];
+        int index =0;
+        for(int i =0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch !=' '){
+                currentword = currentword + ch;
+            }
+    
+            else {
+                if(!currentword.equals("")){
+                    word[index ]=currentword;
+                    currentword="";
+                    index++;
+
+                }
+            }
+        }
+          if(!currentword.equals("")){
+                    word[index ]=currentword;
+                    
+                    index++;
+          }
+          reverse(word,index);
+          String ans ="";
+          for(int i =0;i<index;i++){
+            ans +=word[i];
+            if(i !=index - 1){
+                ans +=" ";
+
+            }
+          }
+          return ans;
     }
 }
