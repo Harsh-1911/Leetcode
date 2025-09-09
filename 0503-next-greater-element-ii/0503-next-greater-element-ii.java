efcbf447d2786f836632ccbe70cb8df5a9358371
Harsh-1911 <hpratap437@gmail.com>
2025-09-09 11:58:55 +0530

    Time: 9 ms (93.06%), Space: 46.9 MB (7.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,26 +1,22 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;;
-        int result[] = new int [n];
-        Stack<Integer> st  = new Stack<>();
+        int n = nums.length;
+        int result[]= new int [n];
+        Stack<Integer> st= new Stack<>(); 
         Arrays.fill(result,-1);
-
         for(int i =0;i<2*n;i++){
             int currentindex = i%n;
-            int currentnums=nums[currentindex];
-            while(!st.isEmpty() && nums[st.peek()]<currentnums){
+            int currentnum = nums[currentindex];
+            while(!st.isEmpty() && nums[st.peek()]<currentnum){
                 int prev = st.pop();
-                result[prev]=currentnums;
-                            }
-                             if(i<n){
-            st.push(currentindex);
-        }
+                result[prev]=currentnum;
+            }
+            if(i<n){
+                st.push(currentindex);
+            }
 
         }
         return result;
-       
-      
-
         
     }
 }
