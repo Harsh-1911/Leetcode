6e7f3022cd2bccfd29d489de032db4915d305f64
Harsh-1911 <hpratap437@gmail.com>
2025-09-14 14:35:08 +0530

    Time: 1 ms (99.97%), Space: 43.8 MB (30.44%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void function(int index,int[]nums,List<Integer> ds ,List<List<Integer>> answer){
+        answer.add(new ArrayList<>(ds));
+        for(int i =index ;i<nums.length;i++){
+            if(i!=index && nums[i]==nums[i-1]){
+                continue;
+            }
+            ds.add(nums[i]);
+            function(i+1,nums,ds,answer);
+            ds.remove(ds.size()-1);
+            
+
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> answer = new ArrayList<>();
+                    function(0,nums,new ArrayList<>(),answer);
+                    return answer;
+
+
+
+        
+    }
+}
