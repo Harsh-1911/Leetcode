1922febc011980065aa5ae184b17756fce77c323
Harsh-1911 <hpratap437@gmail.com>
2025-09-23 13:19:33 +0530

    Time: 274 ms (12.26%), Space: 45.5 MB (7.26%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,25 +1,21 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         String repeated = a;
-        int count= 1;
+        int count=1;
         while(repeated.length()<b.length()){
             repeated +=a;
             count++;
 
-        }
+
+        } 
         if(repeated.contains(b)){
             return count;
-
-
         }
-          repeated +=a;
-            count++;
-            if(repeated.contains(b)){
+        repeated += a;
+        count++;
+        if(repeated.contains(b)){
             return count;
-            }
-            return -1;
-
-
-        
+        }
+        return -1;
     }
 }
